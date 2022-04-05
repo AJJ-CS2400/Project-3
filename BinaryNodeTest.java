862eb55e92a35aebf0a0653d1022ef12b95496c3
Justin <justinh@cpp.edu>
2022-04-05 10:04:05 -0700

    init

---
+++ BinaryNodeTest.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinaryNodeTest {
+
+    @org.junit.jupiter.api.Test
+    void testPostorderTraverse_binaryNodeMethod()
+    {
+        // Arrange
+        // Act
+        // Assert
+    }
+
+    @org.junit.jupiter.api.Test
+    void testGetHeight_binaryNodeMethod()
+    {
+        // Arrange
+        // Act
+        // Assert
+    }
+
+    @org.junit.jupiter.api.Test
+    void testGetNumberOfNodes_binaryNodeMethod()
+    {
+        // Arrange
+        // Act
+        // Assert
+    }
+}
