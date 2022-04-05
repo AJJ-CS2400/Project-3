44e24dd857f80cd602a12789dfcd4719e8a95c9c
Justin <justinh@cpp.edu>
2022-04-05 11:06:19 -0700

    implemented junit test for PostorderTraverse_binaryNodeMethod

--- BinaryNodeTest.java
+++ BinaryNodeTest.java
@@ -1,25 +1,47 @@
+import org.junit.Assert;
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import static org.junit.jupiter.api.Assertions.*;
 
-class BinaryNodeTest {
+public class BinaryNodeTest
+{
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
 
-    @org.junit.jupiter.api.Test
-    void testPostorderTraverse_binaryNodeMethod()
+    @Test
+    public void testPostorderTraverse_binaryNodeMethod()
+    {
+        System.setOut(new PrintStream(outputStreamCaptor));
+        // Arrange
+            BinaryTree<String> aTree = new BinaryTree<>();
+            // Leaves
+            BinaryTree<String> dTree = new BinaryTree<>("D");
+            BinaryTree<String> eTree = new BinaryTree<>("E");
+            BinaryTree<String> gTree = new BinaryTree<>("G");
+
+            // Subtrees:
+            BinaryTree<String> fTree = new BinaryTree<>("F", null, gTree);
+            BinaryTree<String> bTree = new BinaryTree<>("B", dTree, eTree);
+            BinaryTree<String> cTree = new BinaryTree<>("C", fTree, null);
+
+            aTree.setTree("A", bTree, cTree);
+        // Act
+            aTree.postorderTraverse_callBinaryNodeMethod();
+        // Assert
+            Assert.assertEquals("D\r\n" + "E\r\n" + "B\r\n" + "G\r\n" + "F\r\n" + "C\r\n" + "A", outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    public void testGetHeight_binaryNodeMethod()
     {
         // Arrange
         // Act
         // Assert
     }
 
-    @org.junit.jupiter.api.Test
-    void testGetHeight_binaryNodeMethod()
-    {
-        // Arrange
-        // Act
-        // Assert
-    }
-
-    @org.junit.jupiter.api.Test
-    void testGetNumberOfNodes_binaryNodeMethod()
+    @Test
+    public void testGetNumberOfNodes_binaryNodeMethod()
     {
         // Arrange
         // Act
---
+++ BinaryTreeTest.java
@@ -0,0 +1,52 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinaryTreeTest {
+
+    @org.junit.jupiter.api.Test
+    void postorderTraverse()
+    {
+        // Arrange
+        // Act
+        // Assert
+    }
+
+    @org.junit.jupiter.api.Test
+    void postorderTraverse_callBinaryNodeMethod()
+    {
+        // Arrange
+        // Act
+        // Assert
+    }
+
+    @org.junit.jupiter.api.Test
+    void getHeight()
+    {
+        // Arrange
+        // Act
+        // Assert
+    }
+
+    @org.junit.jupiter.api.Test
+    void getHeight_callBinaryNodeMethod()
+    {
+        // Arrange
+        // Act
+        // Assert
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNumberOfNodes()
+    {
+        // Arrange
+        // Act
+        // Assert
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNumberOfNodes_callBinaryNodeMethod()
+    {
+        // Arrange
+        // Act
+        // Assert
+    }
+}
